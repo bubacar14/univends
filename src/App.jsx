df2b78f56f22124a2f44a09cd07d6206806dd57f
Bubacar14 <boubacarndiaye142001@gmail.com>
2024-12-03 00:48:22 +0000

    Remove PrivateRoute and update App.jsx routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Outlet />
     </div>
   );
 }
