7c11211219a82daa62f4d16649899ccc629fd605
Bubacar14 <boubacarndiaye142001@gmail.com>
2024-12-03 01:32:20 +0000

    Update App.jsx to remove authentication-related styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="app">
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
